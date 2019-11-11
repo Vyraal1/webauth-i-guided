c05acf5dec1031cb7c5a60d2ee5dfe57f7480afd
Vyraal1 <kevin.afable@gmail.com>
2019-11-11 12:36:24 -0500

    classwork

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync("B4c0//", salt);
 
-router.get('/', (req, res) => {
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post("/", (req, res) => {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("B4c0//", salt, function(err, hash) {
+      //store hash in password db
+    });
+  });
 });
 
 module.exports = router;
